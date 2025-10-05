5a86ae78a2f4ed05351c80e922108347dcb1b1ca
Asma Jan <bhattisha3115@gmail.com>
2025-10-05 12:10:48 +0530

    Create Sum_of_2_numbers.cpp

---
+++ Sum_of_2_numbers.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x,y,sum=0;
+    cout<<"enter any two numbers\n";
+    cin>>x>>y;
+    sum=x+y;
+    cout<<"sum is\n"<<sum;
+    return 0;
+}
