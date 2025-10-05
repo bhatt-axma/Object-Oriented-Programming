bb07de5a3ad1e15536db4c1d92a3cd73a3899e64
Asma Jan <bhattisha3115@gmail.com>
2025-10-05 12:09:15 +0530

    Create Syntax.cpp

---
+++ Syntax.cpp
@@ -0,0 +1,10 @@
+#include<iostream>   // Preprocessor directive
+
+using namespace std; // Namespace declaration
+
+int main() {  // Main function - execution starts here
+
+    cout << "Hello, world!"; // Output statement
+
+    return 0;        // Exit status
+}
