046abfa3039df23420ee6de0ffdf65f932577bae
Asma Jan <bhattisha3115@gmail.com>
2025-10-05 12:44:29 +0530

    Update Day_Type.cpp

--- Day_Type.cpp
+++ Day_Type.cpp
@@ -2,38 +2,40 @@
 using namespace std;
 
 int main() {
-
     //TEMPERATURE DATA
-    int temperatures[]={65,62,68,74,59,45,41,50,60,67,65,78,82,88,91,92,90,93,87,80,78,79,72,68,61,59};
-
-    int size=sizeof(temperatures)/sizeof(temperatures[0]);
-    int hotdays=0;
-    int pleasantdays=0;
-    int colddays=0;
-
-    cout<<"DAILY TEMPERATURE CATEGORIES\n";
-    for(int i=0;i<size;i++){
+    int temperatures[]={55, 62, 68, 74, 59, 45, 41, 58, 60, 67, 78, 82, 88, 91, 92, 90, 83, 87, 80, 78, 72, 69, 61, 59};
+    
+    int size = sizeof(temperatures)/sizeof(temperatures[0]);
+    int hotDays = 0;
+    int pleasantDays = 0;
+    int coldDays = 0;
+    int sum = 0;
+    cout << "DAILY TEMPERATURE CATEGORIES" << endl;
+    for(int i=0; i < size; i++){
         int temp = temperatures[i];
-        cout<<"day "<<(i+1)<<": "<<temp<<" degrees: ";
-        if(temp>=85){
-            cout<<"hot day\n";
-            hotdays++;
+        sum += temp;
+        cout << "Day" << (i+1) << ": " << temp << " degrees: ";
+        if(temp >= 85){
+            cout << "Hot day!" << endl;
+            hotDays++;
         }
-        else if(temp>=60 && temp<=84){
-            cout<<"pleasant day\n";
-            pleasantdays++;
+        else if(temp >= 60 && temp < 85){
+            cout << "Pleasant day!" << endl;
+            pleasantDays++;
         }
         else{
-            cout<<"cold day\n";
-            colddays++;
+            cout << "Cold day!" << endl;
+            coldDays++;
         }
     }
-
+    
+    double average = static_cast<double>(sum)/size;
     //DISPLAY SUMMARY
-    cout<<"\nSUMMARY:";
-    cout<<"\nhot days: "<<hotdays;
-    cout<<"\npleasant days: "<<pleasantdays;
-    cout<<"\ncold days: "<<colddays;
-
+    cout << "\nSUMMARY\n";
+    cout << "Hot days: " << hotDays << endl;
+    cout << "Pleasant days: " << pleasantDays << endl;
+    cout << "Cold days: " << coldDays << endl;
+    cout << "Average temperature: " << average << " degrees\n";
+    
     return 0;
 }
